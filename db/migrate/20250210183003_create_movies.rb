a512f50778de6f660751b4c8b16ffcc87968ce98
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2025-04-30 08:55:56 -0700

    Had to fix migration, nothing else was changed. Please refer to commit history for proof.

--- db/migrate/20250210183003_create_movies.rb
+++ db/migrate/20250210183003_create_movies.rb
@@ -1,13 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[7.2]
   def change
     create_table :movies do |t|
-      t.string :title, uniqu
+      t.string :title
       t.string :rating
       t.text :description
       t.datetime :release_date
-      t.timestamp
-
-      t.index :title, unique: true
+      t.timestamps
     end
   end
 end
